e97b06e07c47eb1ae4bc8a2c7c7aee3d24a4b517
stark <stark.jc.garcia@gmail.com>
2023-05-26 11:19:34 -0600

    version_1.2 add repair reply app

--- app/src/main/java/com/charros_software/master_guide/screens/MainMenuScreen.kt
+++ app/src/main/java/com/charros_software/master_guide/screens/MainMenuScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
